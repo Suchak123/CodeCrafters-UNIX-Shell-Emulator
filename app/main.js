25f531e6b7340713dfaebf1df7ed82917dbdb503
Suchak123 <niraulasuchak@gmail.com>
2025-05-04 17:41:27 +0545

    type built ins

--- app/main.js
+++ app/main.js
@@ -7,6 +7,7 @@ const rl = readline.createInterface({
 });
 
 // Uncomment this block to pass the first stage
+const shellBuiltins = ["echo", "exit", "type"];
 
 function startREPL(){
 
@@ -16,17 +17,34 @@ function startREPL(){
       return;
     }
 
-    if(answer.startsWith("echo")){
-      const output = answer.split(" ").slice(1).join(" ");
-      console.log(output);
-    } else{
-      console.log(`${answer}: command not found`);
+    const [command, ...args] = answer.trim().split(/\s+/);
+    console.log(command);
+    console.log(args);
+
+    if(command === "echo"){
+      console.log(args.join(" "));
+    }
+    else if(command === "type"){
+      if(args.length === 0) {
+        console.log("type: missing operand");
+      } else{
+        args.forEach((arg) => {
+          if (shellBuiltins.includes(arg)) {
+            console.log(`${arg} is a shell builtin`);
+          } else{
+            console.log(`${arg}: not found`);
+          }
+        });
+      }
+    }
+
+    else {
+      console.log(`${command}: command not found`);
     }
 
     startREPL();
     
   });
-
 }
 
 startREPL();
