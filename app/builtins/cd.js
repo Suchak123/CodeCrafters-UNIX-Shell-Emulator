b87789014d9d72a4cdca71deb1efc84a6d883fc3
Suchak123 <niraulasuchak@gmail.com>
2026-03-02 22:02:01 +0545

    added builtin for home(~)

--- app/builtins/cd.js
+++ app/builtins/cd.js
@@ -8,8 +8,19 @@ module.exports = (args) => {
     return;
   }
 
+  let targetPath;
+
+  if(dir === "~") {
+    targetPath = process.env.HOME || process.env.USERPROFILE;
+  } else if (dir.startsWith("~/")) {
+    const home = process.env.HOME || process.env.USERPROFILE;
+    targetPath = path.join(home, dir.slice(2));
+  } else {
+    targetPath = path.resolve(process.cwd(), dir);
+  }
+
   try {
-    process.chdir(path.resolve(process.cwd(), dir));
+    process.chdir(targetPath);
   } catch {
     console.log(`cd: ${dir}: No such file or directory`);
   }
